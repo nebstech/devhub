9dc6eb87f9859caaf4e2f188a4cd0f9944c6f063
Adonis <adonis.casado@outlook.com>
2024-07-23 03:35:16 -0400

    tailwind

---
+++ src/components/hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div id='hero' className='section'>
+      <h1>Hero Section</h1>
+      <p>Welcome to my Portfolio.</p>
+    </div>
+  );  
+}
+
+export default Hero
